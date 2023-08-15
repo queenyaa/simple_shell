c8f85ada27d8270205f9436919a51720b32623d0
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-08-15 06:24:26 -0700

    displaying errors

--- acc_error1.c
+++ acc_error1.c
@@ -40,7 +40,7 @@ char *err_g_cd(inp_shell *inpsh)
 
 /**
  * err_nfound - generates a generic error message when command is not found
- * inpsh: ...
+ * @inpsh: ...
  * Return: ...
  */
 char *err_nfound(inp_shell *inpsh)
@@ -66,6 +66,76 @@ char *err_nfound(inp_shell *inpsh)
 	_strcat(err, inpsh->argt[0]);
 	_strcat(err, ": not found\n");
 	_strcat(err, "\0");
+	free(v_str);
+
+	return (err);
+}
+
+/**
+ * strcat_cd - function to concatenate error messages related to 'cd'
+ * @inpsh: ...
+ * @note: ...
+ * @err: ...
+ * @v_str: ...
+ * Return: ...
+ */
+char *strcat_cd(inp_shell *inpsh, char *note, char *err, char *v_str)
+{
+	char *red_flag;
+
+	_strcpy(err, inpsh->avec[0]);
+	_strcat(err, ": ");
+	_strcat(err, v_str);
+	_strcat(err, ": ");
+	_strcat(err, inpsh->argt[0]);
+	_strcat(err, note);
+	if (inpsh->argt[1][0] == '-')
+	{
+		red_flag = malloc(3);
+		red_flag[0] = '-';
+		red_flag[1] = inpsh->argt[1][1];
+		red_flag[2] = '\0';
+		_strcat(err, red_flag);
+		free(red_flag);
+	}
+	else
+		_strcat(err, inpsh->argt[1]);
+
+	_strcat(err, "\n");
+	_strcat(err, "\0");
+
+	return (err);
+}
+
+/**
+ * err_exit_shell - error message to display when 'exit' command
+ * is not used appropriately
+ * @inpsh: ...
+ * Return: ...
+ */
+char *err_exit_shell(inp_shell *inpsh)
+{
+	int lgth;
+	char *err;
+	char *v_str;
+
+	v_str = acc_itoa(inpsh->sheep);
+	lgth = _strlen(inpsh->avec[0]) + _strlen(v_str);
+	lgth += _strlen(inpsh->argt[0]) + _strlen(inp->argt[1]) + 23;
+	err = malloc(sizeof(char) * (lgth + 1));
+	if (err == 0)
+	{
+		free(v_str);
+		return (NULL);
+	}
+	_strcpy(err, inpsh->avec[0]);
+	_strcat(err, ": ");
+	_strcat(err, v_str);
+	_strcat(err, ": ");
+	_strcat(err, inpsh->argt[0]);
+	_strcat(err, ": Illegal number: ");
+	_strcat(err, inpsh->argt[1]);
+	_strcat(err, "\n\0");
 	free(v_str);
 
 	return (err);
---
+++ acc_error2.c
@@ -0,0 +1,72 @@
+#include "shell.h"
+
+/**
+ * err_env - generates error message related to 'env' command
+ * @inpsh: ...
+ * Return: ...
+ */
+char *err_env(inp_shell *inpsh)
+{
+	int lgth;
+	char *err;
+	char *v_str;
+	char *note;
+
+	v_str = acc_itoa(inpsh->sheep);
+	note = ": Unable to add/remove from environment\n";
+	lgth = _strlen(inpsh->avec[0]) + _strlen(v_str);
+	lgth += _strlen(inpsh->argt[0]) + _strlen(note) + 4;
+	err = malloc(sizeof(char) * (lgth + 1));
+	if (err == 0)
+	{
+		free(err);
+		free(v_str);
+		return (NULL);
+	}
+
+	_strcpy(err, inpsh->avec[0]);
+	_strcat(err, ": ");
+	_strcat(err, v_str);
+	_strcat(err, ": ");
+	_strcat(err, inpsh->argt[0]);
+	_strcat(err, note);
+	_strcat(err, "\0");
+	free(v_str);
+
+	return (err);
+}
+
+/**
+ * err_path_126 - generates error message for when an execution
+ * of a command's path is invalid or permission is denied
+ * @inpsh: ...
+ * Return: ...
+ */
+char *err_path_126(inp_shell *inpsh)
+{
+	int lgth;
+	char *v_str;
+	char *err;
+
+	v_str = acc_itoa(inpsh->sheep);
+	lgth = _strlen(inpsh->avec[0]) + _strlen(v_str);
+	lgth += _strlen(inpsh->argt[0]) + 24;
+	err = malloc(sizeof(char) + (lgth + 1));
+	if (err == 0)
+	{
+		free(err);
+		free(v_str);
+		return (NULL);
+	}
+
+	_strcpy(err, inpsh->avec[0]);
+	_strcat(err, ": ");
+	_strcat(err, v_str);
+	_strcat(err, ": ");
+	_strcat(err, inpsh->argt[0]);
+	_strcat(err, ": Permission denied\n");
+	_strcat(err, "\0");
+	free(v_str);
+
+	return (err);
+}
