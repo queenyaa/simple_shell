6c1c9fdb04b296e24f8618f79a55d0f1b6c399b9
eoasante98 <eo.asante3@gmail.com>
2023-08-19 16:43:54 -0700

    adding g_builtin.c

---
+++ g_builtin.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * get_builtin - builtin that pais the command in the arg
+ * @cmd: command
+ * Return: function pointer of the builtin command
+ */
+int (*get_builtin(char *cmd))(inp_shell *)
+{
+	inher_u builtin[] = {
+		{ "env", env },
+		{ "exit", exit_sh },
+		{ "setenv", _setenv },
+		{ "unsetenv", _unsetenv },
+		{ "cd", cd_shell },
+		{ "help", g_help },
+		{ NULL, NULL }
+	};
+	int x;
+
+	for (x = 0; builtin[x].nick; x++)
+	{
+		if (_strcmp(builtin[x].nick, cmd) == 0)
+			break;
+	}
+
+	return (buitlin[x].pf);
+}
